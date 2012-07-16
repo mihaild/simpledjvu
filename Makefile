c4b53a01c1cedeaaeb63db78cb9f50ce3e136df1
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2012-07-16 18:16:02 +0200

    making hystograms

--- Makefile
+++ Makefile
@@ -1,37 +1,37 @@
 all: bitonize remove_background decrease_colors_count bitonize_threshold hystograms
 
 bitonize: bitonize.o pgm.o
-	g++ -O3 -std=c++0x -o bitonize bitonize.o pgm.o
+	g++ -O3 --debug -std=c++0x -o bitonize bitonize.o pgm.o
 
 remove_background: remove_background.o pgm.o
-	g++ -O3 -std=c++0x -o remove_background remove_background.o pgm.o
+	g++ -O3 --debug -std=c++0x -o remove_background remove_background.o pgm.o
 
 decrease_colors_count: decrease_colors_count.o pgm.o
-	g++ -O3 -std=c++0x -o decrease_colors_count decrease_colors_count.o pgm.o
+	g++ -O3 --debug -std=c++0x -o decrease_colors_count decrease_colors_count.o pgm.o
 
 bitonize_threshold: bitonize_threshold.o pgm.o
-	g++ -O3 -std=c++0x -o bitonize_threshold bitonize_threshold.o pgm.o
+	g++ -O3 --debug -std=c++0x -o bitonize_threshold bitonize_threshold.o pgm.o
 
 hystograms: hystograms.o pgm.o
-	g++ -O3 -std=c++0x -o hystograms hystograms.o pgm.o
+	g++ -O3 --debug -std=c++0x -o hystograms hystograms.o pgm.o
 
 bitonize.o: bitonize.cpp types.h constants.h
-	g++ -O3 -std=c++0x -c -o bitonize.o bitonize.cpp
+	g++ -O3 --debug -std=c++0x -c -o bitonize.o bitonize.cpp
 
 remove_background.o: remove_background.cpp types.h pgm.h constants.h
-	g++ -O3 -std=c++0x -c -o remove_background.o remove_background.cpp
+	g++ -O3 --debug -std=c++0x -c -o remove_background.o remove_background.cpp
 
 decrease_colors_count.o: decrease_colors_count.cpp types.h pgm.h constants.h
-	g++ -O3 -std=c++0x -c -o decrease_colors_count.o decrease_colors_count.cpp
+	g++ -O3 --debug -std=c++0x -c -o decrease_colors_count.o decrease_colors_count.cpp
 
 bitonize_threshold.o: bitonize_threshold.cpp types.h pgm.h constants.h
-	g++ -O3 -std=c++0x -c -o bitonize_threshold.o bitonize_threshold.cpp
+	g++ -O3 --debug -std=c++0x -c -o bitonize_threshold.o bitonize_threshold.cpp
 
 hystograms.o: hystograms.cpp types.h pgm.h constants.h
-	g++ -O3 -std=c++0x -c -o hystograms.o hystograms.cpp
+	g++ -O3 --debug -std=c++0x -c -o hystograms.o hystograms.cpp
 
 pgm.o: pgm.cpp types.h constants.h
-	g++ -O3 -std=c++0x -c -o pgm.o pgm.cpp
+	g++ -O3 --debug -std=c++0x -c -o pgm.o pgm.cpp
 
 clean:
 	rm -f pgm.o bitonize.o remove_background.o decrease_colors_count.o hystograms.o bitonize remove_background decrease_colors_count bitonize_threshold hystograms
--- hystograms.cpp
+++ hystograms.cpp
@@ -1,10 +1,18 @@
 #include <vector>
 #include <array>
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <stdio.h>
 #include "types.h"
 #include "constants.h"
+#include "pgm.h"
 
 using std::vector;
 using std::array;
+using std::fill;
+
+using std::cout;
 
 Hystogram operator -(const Hystogram &a, const Hystogram &b) {
     Hystogram result(a);
@@ -14,11 +22,16 @@ Hystogram operator -(const Hystogram &a, const Hystogram &b) {
     return result;
 }
 
-Hystogram operator +(const Hystogram &a, const Hystogram &b) {
-    Hystogram result;
+Hystogram& operator +=(Hystogram &a, const Hystogram &b) {
     for (int i = 0; i < COLORS_COUNT; ++i) {
-        result[i] += b[i];
+        a[i] += b[i];
     }
+    return a;
+}
+
+Hystogram operator +(const Hystogram &a, const Hystogram &b) {
+    Hystogram result(a);
+    result += b;
     return result;
 }
 
@@ -26,13 +39,13 @@ vector<vector<Hystogram> > get_rectangle_hystograms(byte *pixels, int row_size, 
     vector<vector<Hystogram> > rectangles_hystogram(rows_count, vector<Hystogram> (row_size));
     ++rectangles_hystogram[0][0][pixels[0]];
     for (int i = 0; i < rows_count; ++i) {
-        if (i != 0) {
-            rectangles_hystogram[i] = rectangles_hystogram[i-1];
-        }
+        Hystogram cline;
+        fill(cline.begin(), cline.end(), 0);
         for (int j = 0; j < row_size; ++j) {
-            if (j != 0) {
-                rectangles_hystogram[i][j] = rectangles_hystogram[i][j-1];
-                ++rectangles_hystogram[i][j][pixels[i * row_size + j]];
+            ++cline[pixels[i * row_size + j]];
+            rectangles_hystogram[i][j] = cline;
+            if (i != 0) {
+                rectangles_hystogram[i][j] += rectangles_hystogram[i-1][j];
             }
         }
     }
@@ -40,16 +53,16 @@ vector<vector<Hystogram> > get_rectangle_hystograms(byte *pixels, int row_size, 
 }
 
 // fails if radius > min(row_size, rows_count)
-vector<vector<Hystogram> > get_local_hystograms(const vector<vector<Hystogram> > &rectangles_hystogram, int radius) {
-    vector<vector<Hystogram> > result(rectangles_hystogram);
-    for (int i = 0; i < rectangles_hystogram.size(); ++i) {
-        int left = ((i - radius/2) > 0) ? (i - radius/2) : 0;
-        for (int j = 0; j < rectangles_hystogram[i].size(); ++j) {
-            int top = ((j - radius/2) > 0) ? (j - radius/2) : 0;
-            result[i][j] = rectangles_hystogram[left][top] + rectangles_hystogram[left + radius][top + radius] - rectangles_hystogram[left][top + radius] - rectangles_hystogram[left + radius][top];
-        }
+Hystogram get_local_hystogram(const vector<vector<Hystogram> > &rectangles_hystogram, int radius, int x, int y) {
+    int left = ((x - radius/2) > 0) ? (x - radius/2) : 0;
+    if ((left + radius) >= rectangles_hystogram.size()) {
+        left = rectangles_hystogram.size() - radius - 1;
     }
-    return result;
+    int top = ((y - radius/2) > 0) ? (y - radius/2) : 0;
+    if ((top + radius) >= rectangles_hystogram[left].size()) {
+        top = rectangles_hystogram[left].size() - radius - 1;
+    }
+    return rectangles_hystogram[left][top] + rectangles_hystogram[left + radius][top + radius] - rectangles_hystogram[left][top + radius] - rectangles_hystogram[left + radius][top];
 }
 
 LevelsDistribution get_levels_distribution(const Hystogram &hystogram) {
@@ -61,6 +74,77 @@ GlobalLevelsDistribution get_global_levels_distribution(byte *pixels, int row_si
     //vector<vector<Hystogram> > hystograms = get_hystograms(pixels, row_size, rows_count, radius);
 }
 
-int main(int argc, char *argw[]) {
+int main(int argc, char *argv[]) {
+    FILE *data;
+
+    data = fopen(argv[1], "rb");
+
+    byte *pixels;
+    int32 width, height, row_size, rows_count;
+
+    load_pgm(data, &width, &height, &row_size, &rows_count, &pixels, 0);
+    fclose(data);
+
+    int radius = atoi(argv[2]);
+
+    vector<vector<Hystogram> > rectangle_hystograms = get_rectangle_hystograms(pixels, row_size, rows_count);
+    /*for (int k = 203; k < 204; ++k) {
+        cout << k << ' ';
+        cout << rectangle_hystograms[0][0][k] << ' ';
+        cout << rectangle_hystograms[radius][0][k] << ' ';
+        cout << rectangle_hystograms[0][radius][k] << ' ';
+        cout << rectangle_hystograms[radius][radius][k] << ' ';
+        cout << get_local_hystogram(rectangle_hystograms, radius, 25, 25)[k] << ' ';
+        cout << '\n';
+    }
+    return 0;*/
+
+    std::cout << "hystograms OK\n";
+
+    int max_hysto = 0;
+    for (int i = radius / 2; i < width; i += radius) {
+        for (int j = radius / 2; j < height; j += radius) {
+            Hystogram hystogram = get_local_hystogram(rectangle_hystograms, radius, i, j);
+            for (int k = 0; k < COLORS_COUNT; ++k) {
+                max_hysto = max_hysto > hystogram[k] ? max_hysto : hystogram[k];
+            }
+        }
+    }
+
+    byte *colors = new byte[COLORS_COUNT*max_hysto*4];
+    for (int i = radius / 2; i < width; i += radius) {
+        for (int j = radius / 2; j < height; j += radius) {
+            for (int k = 0; k < COLORS_COUNT*max_hysto*4; ++k) {
+                colors[k] = 0;
+            }
+            std::cout << i << ' ' << j << "\n";
+            Hystogram hystogram = get_local_hystogram(rectangle_hystograms, radius, i, j);
+            for (int k = 0; k < COLORS_COUNT; ++k) {
+                if (hystogram[k]) {
+                    cout << k << ' ' << hystogram[k] << '\n';
+                }
+                for (int l = 0; l < hystogram[k]; ++l) {
+                    colors[k*max_hysto*2 + l*2] = 
+                        colors[k*max_hysto*2 + l*2 + 1] =
+                        colors[(k+1)*max_hysto*2 + l*2] =
+                        colors[(k+1)*max_hysto*2 + l*2 + 1] =
+                        1;
+                }
+            }
+            std::cout << "ok\n";
+
+            char fname[255];
+            sprintf(fname, "test_hystograms/%s_%d_%d.pbm", argv[3], i, j);
+            std::cout << fname << '\n';
+            FILE *out(fopen(fname, "wb"));
+            std::cout << "saving...\n";
+            save_pbm(out, colors, max_hysto*2, COLORS_COUNT*2, max_hysto*2, COLORS_COUNT*2);
+            cout << "closing...\n";
+            fclose(out);
+            std::cout << "saved\n";
+        }
+    }
+    delete colors;
+
     return 0;
 }
