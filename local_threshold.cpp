bd83f763dbcb7ac0a18ad6207040b4558bc932db
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2012-07-19 11:31:29 +0200

    local_threshold parameters

--- local_threshold.cpp
+++ local_threshold.cpp
@@ -24,15 +24,26 @@ byte get_right_quantile(const Hystogram &hystogram, double level) {
 int main(int argc, char *argv[]) {
     FILE *data;
 
-    data = fopen(argv[1], "rb");
+    char *input, *output;
+    int radius;
+    double level;
+    if (argc < 3 || argc > 5) {
+        std::cerr << "Wrong usage\n";
+        return 1;
+    }
+    input = argv[1];
+    output = argv[2];
+
+    radius = (argc >= 4) ? atoi(argv[3]) : 20;
+    level = (argc >= 5) ? atof(argv[4]) : 0.05;
+
+    data = fopen(input, "rb");
 
     byte *pixels;
     int32 width, height, row_size, rows_count;
 
     load_pgm(data, &width, &height, &row_size, &rows_count, &pixels, 0);
     fclose(data);
-
-    int radius = atoi(argv[2]);
 
     vector<vector<Hystogram> > rectangle_hystograms = get_rectangle_hystograms(pixels, row_size, rows_count);
 
@@ -42,7 +53,7 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < width; ++j) {
             //std::cout << i << ' ' << j << ' ' << int(get_right_quantile(get_local_hystogram(rectangle_hystograms, radius, i, j), 0.5)) << ' ';
             //std::cout << int(get_right_quantile(get_local_hystogram(rectangle_hystograms, radius, i, j), 0.5)) << ' ';
-            colors[i*width + j] = get_right_quantile(get_local_hystogram(rectangle_hystograms, radius, i, j), 0.05);
+            colors[i*width + j] = get_right_quantile(get_local_hystogram(rectangle_hystograms, radius, i, j), level);
             //colors[i*width + j] = std::min(255, get_local_hystogram(rectangle_hystograms, radius, i, j)[150]);
             //colors[i*width + j] = j % 256;
         }
@@ -50,7 +61,7 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << "saving\n";
-    save_pgm(fopen(argv[3], "wb"), colors, width, height);
+    save_pgm(fopen(output, "wb"), colors, width, height);
 
     return 0;
 }
